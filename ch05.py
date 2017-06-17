9f0f1e2ca606a62a45c361e6b43f88ecf0c3b02c
ChengAndy <chengandy@ChengAndydeMacBook-Pro.local>
2017-06-17 21:37:04 +0800

    p.156

---
+++ ch05.py
@@ -0,0 +1,60 @@
+# ch05
+# Support Vector Machine
+import numpy as np
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
+
+
+# Iris dataset
+iris = datasets.load_iris()
+X = iris['data'][:, (2, 3)]
+y = (iris['target'] == 2).astype(np.float64)
+
+svm_clf = Pipeline([
+    ('scaler', StandardScaler()),
+    ('linear_svc', LinearSVC(C=1, loss='hinge'))
+])
+
+svm_clf.fit(X, y)
+
+
+# Moons dataset
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import PolynomialFeatures
+
+moon = make_moons(n_samples=10000, noise=0.15, random_state=0)
+plt.scatter(moon[0][:, 0], moon[0][:, 1], c=moon[1])
+plt.show()
+
+X = moon[0]
+y = moon[1]
+
+polynomial_svm_clf = Pipeline([
+    ('poly_features', PolynomialFeatures(degree=3)),
+    ('scaler', StandardScaler()),
+    ('svm_clf', LinearSVC(C=10, loss='hinge'))
+])
+
+polynomial_svm_clf.fit(X, y)
+
+
+# Polynomial Kernel
+from sklearn.svm import SVC
+poly_kernel_svm_clf = Pipeline([
+    ('scaler', StandardScaler()),
+    ('svm_clf', SVC(kernel='poly', degree=3, coef0=1, C=5))
+])
+poly_kernel_svm_clf.fit(X, y)
+
+
+# Gaussian RBF Kernel
+rbf_kernel_svm_clf = Pipeline([
+    ('scaler', StandardScaler()),
+    ('svm_clf', SVC(kernel='rbf', gamma=5, C=0.001))
+])
+rbf_kernel_svm_clf.fit(X, y)
+
+
