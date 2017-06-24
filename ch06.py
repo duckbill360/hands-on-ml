86d2ff088e51db4e78ccf95ce82a9649209e6b14
Po-Wei Cheng <duckbill360@users.noreply.github.com>
2017-06-24 20:05:27 +0800

    ch06

--- ch06.py
+++ ch06.py
@@ -12,9 +12,10 @@ y = iris.target
 tree_clf = DecisionTreeClassifier(max_depth=2)
 tree_clf.fit(X, y)
 
-print(tree_clf.predict_proba([5, 1.5]))
-print(tree_clf.predict([[5, 1.5]]))
+from sklearn.tree import DecisionTreeRegressor
 
+tree_reg = DecisionTreeRegressor(max_depth=2)
+tree_reg.fit(X, y)
 
 
 
