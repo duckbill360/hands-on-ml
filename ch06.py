a9e4387c2549a3180a405c2a1decba94dcc89d6c
ChengAndy <chengandy@ChengAndydeMacBook-Pro.local>
2017-06-24 10:23:26 +0800

    ch06

---
+++ ch06.py
@@ -0,0 +1,27 @@
+# ch06
+# Decision Trees
+
+# DecisionTreeClassifier
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+iris = load_iris()
+X = iris.data[:, 2:] # petal length and width
+y = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2)
+tree_clf.fit(X, y)
+
+print(tree_clf.predict_proba([5, 1.5]))
+print(tree_clf.predict([[5, 1.5]]))
+
+
+
+
+
+
+
+
+
+
+
